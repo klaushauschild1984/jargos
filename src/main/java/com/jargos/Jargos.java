cf322c2ffe2bb67b58456686e28742ae7195ba8a
Klaus Hauschild <klaus.hauschild.1984@gmail.com>
2018-08-19 20:09:35 +0200

    initial commit

---
+++ src/main/java/com/jargos/Jargos.java
@@ -0,0 +1,11 @@
+package com.jargos;
+
+public class Jargos {
+
+    public static void main(final String[] args) {
+        System.out.println("Jargos");
+        System.out.println("---");
+        System.out.println("Argos from Java (out)");
+    }
+
+}
